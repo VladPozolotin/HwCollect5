a377170c3c255f389cc95b71935b77409a71efd7
VladPozolotin <vlad.yugra.nyagan@gmail.com>
2023-03-28 18:10:44 +0300

    lambda comparator

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
-        PersonComparator corp = new PersonComparator(3);
+        Comparator<Person> corp = (o1, o2) -> {
+            int comp = 0;
+            if (!Boolean.TRUE.equals((o1.getSurname().split("-")).length >= 3 && (o2.getSurname().split("-")).length >= 3)) {
+                comp = Integer.compare((o1.getSurname().split("-")).length, (o2.getSurname().split("-")).length);
+            }
+            if (comp == 0) {
+                comp = Integer.compare(o1.getAge(), o2.getAge());
+            }
+            return comp;
+        };
         list.add(new Person("Александр", "Суворов-Рымникский", 69));
         list.add(new Person("Фёдор", "Орлов-Денисов", 48));
         list.add(new Person("Василий", "Татищев", 52));
